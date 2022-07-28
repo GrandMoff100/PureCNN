dc93a396f772a4ef5d78a50ebcc054894d118f3a
Nate <minecraftcrusher100@gmail.com>
2022-07-28 04:40:54 +0000

    Add fcnn usage

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import pandas as pd
-from cnn.net import ConvolutionNeuralNetwork
+import numpy as np
+from cnn.net import Network, ConvolutionalLayer, FCLayer, MaxPool, FlattenLayer
 
 
 df = pd.read_csv("digits.csv", comment="#")
-df["label"] = df["label"].astype("category")
+
+
+def one_hot(label: str) -> np.ndarray:
+    """Convert a label to a one-hot vector."""
+    return np.array([1 if i == label else 0 for i in "0123456789"])
+
+
+df["label"] = df["label"].apply(one_hot)
+
+data = df.drop(columns=["label"]).to_numpy()
+labels = df["label"].to_numpy()
+
+network = Network(
+    [
+        FCLayer(784, 48),
+        FCLayer(48, 10),
+    ]
+)
+
+
+network.train(data, labels, iterations=1000, batch_size=10)
